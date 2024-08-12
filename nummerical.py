a38d59050f5da18abdb243329a32d7873a82c3f9
Daniil_Tchetyra <defender.0599@gmail.com>
2024-08-12 18:24:34 +0300

    Add files via upload

---
+++ nummerical.py
@@ -0,0 +1,17 @@
+# Переменная для количества выполненных ДЗ
+completedAssignments = 12
+
+# Переменная для количества затраченных часов
+hoursSpent = 1.5
+
+# Переменная для названия курса
+courseName = 'Python'
+
+# Вычисление времени на одно задание ответ 0.125
+timePerAssignment = hoursSpent / completedAssignments
+
+# Вывод результатов
+print("Количество выполненных ДЗ:", completedAssignments)
+print("Количество затраченных часов:", hoursSpent)
+print("Название курса:", courseName)
+print("Время на одно задание:", timePerAssignment)
